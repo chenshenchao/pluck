929c5a7a266b308af49ab0d9f51fbf40d377aa8a
chenshenchao <chenshenchao@outlook.com>
2018-02-05 01:52:21 +0800

    add sql.

---
+++ library/Sql.php
@@ -0,0 +1,10 @@
+<?php namespace pluck;
+
+/**
+ * SQL
+ * 
+ * 提供 SQL 的相关方法。
+ */
+final class Sql {
+
+}
---
+++ library/facade/Sql.php
@@ -0,0 +1,32 @@
+<?php namespace pluck\facade;
+
+use think\Facade;
+
+/**
+ * Sql 的 Facade
+ * 
+ * 提供执行 SQL 文件的静态方法。
+ */
+final class Sql extends Facade {
+
+    /**
+     * 执行 SQL 文件。
+     * 
+     * @param string $name: 文件名。
+     */
+    public static function execute($name, $data=null) {
+        $path = Path::of('sql', $name.'sql');
+        $text = file_get_contents($path);
+        $code = preg_replace('/--.*?\n/', '', $text);
+        // 文本替换
+        if (isset($data) and is_array($data)) {
+            $keys = array_keys($data);
+            $values = array_values($data);
+            $code = str_replace($keys, $values, $code);
+        }
+        $statements = array_filter(explode(';', $code));
+        foreach ($statements as $statement) {
+            \Db::execute($statement);
+        }
+    }
+}
